1e3af035e25ebe45d8a69d5f9cc1d2368c46a0cd
Nicknyr <nick@Nicks-MacBook-Pro.local>
2020-01-06 21:54:40 -0500

    first commit. At Build Your Interface with Polaris - Add a settings toggle section in tutorial

---
+++ .next/static/webpack/static/development/pages/annotated-layout.js.a2bd8431a496bd5e502b.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate("static/development/pages/annotated-layout.js",[
+/* 0 */,
+/* 1 */,
+/* 2 */
+false,
+/* 3 */
+/*!***************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fannotated-layout&absolutePagePath=%2FUsers%2Fnick%2FDocuments%2FProjects%2Fshopify%2Fpages%2Fannotated-layout.js ***!
+  \***************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fannotated-layout&absolutePagePath=%2FUsers%2Fnick%2FDocuments%2FProjects%2Fshopify%2Fpages%2Fannotated-layout.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fannotated-layout&absolutePagePath=%2FUsers%2Fnick%2FDocuments%2FProjects%2Fshopify%2Fpages%2Fannotated-layout.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=annotated-layout.js.a2bd8431a496bd5e502b.hot-update.js.map
